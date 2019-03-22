9e116e0bd981ed5e86c95987dc5567620dd8c324
Bryce Corbitt <bscorbitt@wpi.edu>
2019-03-21 21:30:48 -0400

    Updated config, included ova used in submission

--- Gateway VM/config/bootlocal.sh
+++ Gateway VM/config/bootlocal.sh
@@ -2,6 +2,10 @@
 # put other system startup commands here
 /usr/local/etc/init.d/openssh start &
 /opt/eth1.sh &
+sudo syslogd
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
 sudo dnsmasq
