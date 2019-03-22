5a9f9ee6c24f2ff7d608944f1ec31fbb81dade48
Bryce Corbitt <bscorbitt@wpi.edu>
2019-03-22 15:12:56 -0400

    substantial progress in packet analysis program

--- Packet Analysis/packet_read.py
+++ Packet Analysis/packet_read.py
@@ -1,16 +1,52 @@
 import pyshark
 
-INTERFACE = 'enp0s31f6'
-BURST_MILLIS = 1000
-READ_INTERVAL = 1 #number of packets to read in each loop iteration
-capture = pyshark.LiveCapture(interface=INTERFACE)
+INTERFACE = 'wlp3s0'
+BURST_SECONDS = 1
+READ_SECONDS = 2
 
 
-def run():
-    capture.sniff(packet_count=READ_INTERVAL)
-    print(capture[0])
-    #for packet in capture.sniff_continuously(packet_count=1):
-    #    print("Just arrived! " + str(packet))
+def check_burst(cap, start_index=0):
+    for i in range(start_index, len(cap)-1):
+        pkt_delta = float(cap[i+1].sniff_timestamp) - float(cap[i].sniff_timestamp)
+        if pkt_delta >= BURST_SECONDS:
+            return i
+    return -1
 
 
-run()
+def print_analysis(pkts):
+    for pkt in pkts:
+        timestamp = pkt.sniff_time
+        src = pkt.ip.src
+        dst = pkt.ip.dst
+        src_port = pkt.layers[2].srcport
+        dst_port = pkt.layers[2].dstport
+        protocol = pkt.layers[-1].layer_name
+
+        print(str(timestamp) + " " + src + " " + dst + " " + src_port + " " + dst_port + " " + protocol)
+
+
+def run(interface="eth1"):
+    capture = pyshark.LiveCapture(interface=INTERFACE, display_filter='ip and tcp or udp')
+    pkt_index = 0
+    current_burst = []
+
+    # main loop
+    while True:
+
+        # This loop will keep capturing packets until a burst interval is detected
+        while check_burst(capture, pkt_index) == -1:
+            capture.sniff(timeout=READ_SECONDS)
+
+        end_burst = check_burst(capture, pkt_index) + 1
+        current_burst = capture[pkt_index:end_burst]
+        pkt_index = end_burst
+
+        # do analysis with current burst...
+        print_analysis(current_burst)
+
+
+run(interface=INTERFACE)
+
+
+
+
