447abf9df6ba00ae3315aabe1e96056eb9c0ff92
Bryce Corbitt <bscorbitt@wpi.edu>
2019-03-28 11:53:16 -0400

    Finalized for submission

--- Packet Analysis/packet_read.py
+++ Packet Analysis/packet_read.py
@@ -1,8 +1,6 @@
 import pyshark
 import os
-INTERFACE = 'wlp3s0'
 BURST_SECONDS = 1
-READ_SECONDS = 1
 
 IDENTIFIER_KEYS = ['src_port', 'dst_port', 'protocol']
 null_fds = [os.open(os.devnull, os.O_RDWR) for x in range(2)]
@@ -12,6 +10,7 @@ burst_strings = []
 last_pkt = None
 
 
+# compares with previous packet read. Returns True if time difference >= 1 second
 def check_burst(pkt):
     global last_pkt
     if not last_pkt:
@@ -22,7 +21,7 @@ def check_burst(pkt):
     if pkt_delta >= BURST_SECONDS:
         last_pkt = pkt
         return True
-    last_pkt = pkt
+    last_pkt = pkt  # store packet to be used for checking next packet
     return False
 
 
@@ -37,17 +36,19 @@ def packet_extract(pkt):
 
     protocol = str(pkt.layers[2].layer_name)
     length = int(pkt.length)
+    # return the information we need as a dictionary
     return {'timestamp': timestamp, 'src': src, 'dst': dst, 'src_port': src_port, 'dst_port': dst_port,
             'protocol': protocol, 'outbound': outbound, 'length': length}
 
 
+# generates a string to be used for indexing in dictionary
 def packet_serialize(pkt_dict):
     val = ''
     if not pkt_dict['outbound']:
         val += pkt_dict['src']
         val += pkt_dict['dst_port']
         val += pkt_dict['src_port']
-    else:
+    else:  # inbound data needs the same indexing as outbound data, so we reverse the source and destination
         val += pkt_dict['dst']
         val += pkt_dict['src_port']
         val += pkt_dict['dst_port']
@@ -57,6 +58,7 @@ def packet_serialize(pkt_dict):
     return val
 
 
+# return pkt_dict info + pkt_stats collected for its index as a string to print
 def get_analysis(pkt, index):
     return str(pkt['timestamp'] + " " + pkt['src'] + " " + pkt['dst'] + " " + pkt['src_port'] + " " + pkt['dst_port'] +
                " " + pkt['protocol'] + " " + str(pkt_stats[index]['pkts_sent']) + " " +
@@ -68,7 +70,8 @@ def run(interface="eth1"):
     global burst_strings
     capture = pyshark.LiveCapture(interface=interface, display_filter='ip and tcp or udp')
 
-    for pkt in capture.sniff_continuously():
+    for pkt in capture.sniff_continuously():  # for each packet received...
+        # extract packet information, add it to statistics dictionary
         entries = pkt_stats.keys()
         pkt_dict = packet_extract(pkt)
         index = packet_serialize(pkt_dict)
@@ -82,21 +85,25 @@ def run(interface="eth1"):
             pkt_stats[index]['pkts_received'] += 1
             pkt_stats[index]['bytes_received'] += pkt_dict['length']
 
-        burst_strings.append(get_analysis(pkt_dict,index))
-
+        # if a burst is detected, print all stored packet strings and clear the list
         if check_burst(pkt):
-            # print("BURST!!!!\n")
             for line in burst_strings:
                 print(line)
             burst_strings = []
+
+        # instead of storing packet until burst is detected, store the string that is to be printed for memory
+        # efficiency.
+        burst_strings.append(get_analysis(pkt_dict, index))
 
 
 if __name__ == '__main__':
     try:
         run()
     except KeyboardInterrupt:
-        for line in burst_strings:
-            print(line)
+        # print remaining packets that would've been used for next burst
+        print()
+        for l in burst_strings:
+            print(l)
 
         # Used to suppress errors on KeyboardInterrupt
         os.dup2(null_fds[0], 1)
