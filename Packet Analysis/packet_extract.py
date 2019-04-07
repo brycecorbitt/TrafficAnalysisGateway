a816c11350b3573f5266d43bb6b49292f2b40468
Bryce Corbitt <bscorbitt@wpi.edu>
2019-04-07 19:12:21 -0400

    Warning suppression and minor changes

--- Packet Analysis/packet_extract.py
+++ Packet Analysis/packet_extract.py
@@ -1,6 +1,8 @@
 import pyshark
 import numpy as np
-
+import os
+null_fds = [os.open(os.devnull, os.O_RDWR) for x in range(2)]
+save = os.dup(1), os.dup(2)
 """"
 Explanation of display filter:
 'ip' filters out ipv6 traffic
@@ -10,7 +12,7 @@ Explanation of display filter:
 """
 DISPLAY_FILTER = 'ip and not arp and !(udp.port == 53 or udp.port == 5353 or udp.port == 17) and !(eth.dst[0] & 1)'
 
-PACKET_PATH = '../GatewayVM/recorded_traffic'
+
 
 
 # Extract the time between packets (in seconds) with pkt_b following after pkt_a
@@ -22,6 +24,8 @@ def pkt_delta(pkt_a, pkt_b):
 
 # Extracts the distance (in seconds) between each packet and their lengths in bytes
 def extract_pkt(path):
+    os.dup2(null_fds[0], 1)
+    os.dup2(null_fds[1], 2)
     cap = pyshark.FileCapture(path, display_filter=DISPLAY_FILTER)
     lengths = []
     times = []
@@ -33,9 +37,11 @@ def extract_pkt(path):
         lengths.append(length)
         prev = pkt
 
+    os.dup2(save[0], 1)
+    os.dup2(save[1], 2)
     return np.concatenate(([times], [lengths]), axis=0)
 
 
-# test = extract_pkt(PACKET_PATH + '/browser.pcap')
+# test = extract_pkt('../GatewayVM/recorded_traffic/browser.pcap')
 # print(np.shape(test))
 # print(test[:,1])
