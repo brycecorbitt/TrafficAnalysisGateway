989357932d32f9df41af2b777754f2fa4de1196c
Bryce Corbitt <bscorbitt@wpi.edu>
2019-04-19 15:35:31 -0400

    added more test code

--- Packet Analysis/classifyFlows.py
+++ Packet Analysis/classifyFlows.py
@@ -7,9 +7,9 @@ from sklearn.ensemble import RandomForestClassifier
 import random
 import itertools
 
-PACKET_PATH = '../GatewayVM/recorded_traffic/'
-TEST_PATH = '../GatewayVM/recorded_traffic/test_data/'
-TRAINED_PATH = '../GatewayVM/recorded_traffic/trained.forest'
+PACKET_PATH = 'recorded_traffic/training_data/'
+TRAINED_PATH = 'recorded_traffic/trained.forest'
+TEST_PATH = 'recorded_traffic/test_data/'
 
 training_captures = ['browser', 'youtube', 'weather_channel', 'google_news', 'fruit_ninja']
 
@@ -25,48 +25,71 @@ def format_sample(sample_caps):
     return formatted, lbls
 
 
-if __name__ == '__main__':
-    training_sample = []
-    if(not os.path.exists(TRAINED_PATH)):
-        # Get training sample data for each pcap file
-        for cap in training_captures:
-            path = PACKET_PATH + cap + '.pcap'
-            print(path)
-            data = packet_extract.extract_pkt(path)
-            training_sample.append(data)
-        with open(TRAINED_PATH, 'wb') as f:
-            pickle.dump(training_sample, f)
-
-    else:
-        with open(TRAINED_PATH, 'rb') as f:
-            training_sample = pickle.load(f)
+training_sample = []
+if not os.path.exists(TRAINED_PATH):
+    # Get training sample data for each pcap file
+    for cap in training_captures:
+        path = PACKET_PATH + cap + '.pcap'
+        print(path)
+        data = packet_extract.extract_pkt(path)[0]
+        training_sample.append(data)
 
     training, labels = format_sample(training_sample)
-    rf = RandomForestClassifier(n_estimators=100, max_depth=7)
 
     print("Start Training!!!")
+    s = [4, 1, 2, 4, 5]
+    rf = RandomForestClassifier(n_estimators=100, max_depth=7,
+                                class_weight={0: s[0], 1: s[1], 2: s[2], 3: s[3], 4: s[4]})
     rf.fit(training, labels)
+    with open(TRAINED_PATH, 'wb') as f:
+        pickle.dump(rf, f)
+
+else:
+    with open(TRAINED_PATH, 'rb') as f:
+        rf = pickle.load(f)
+        print("Loaded trained classifier from file!")
+
+
+def get_label(feature_vector):
+    label = rf.predict(feature_vector)
+    return training_captures[int(label[0])]
+
+
+if __name__ == '__main__':
+    # training_sample = []
+    # if not os.path.exists(TRAINED_PATH):
+    #     # Get training sample data for each pcap file
+    #     for cap in training_captures:
+    #         path = PACKET_PATH + cap + '.pcap'
+    #         print(path)
+    #         data = packet_extract.extract_pkt(path)
+    #         training_sample.append(data)
+    #     with open(TRAINED_PATH, 'wb') as f:
+    #         pickle.dump(training_sample, f)
+    #
+    # else:
+    #     with open(TRAINED_PATH, 'rb') as f:
+    #         training_sample = pickle.load(f)
+    #
+    # training, labels = format_sample(training_sample)
+    # rf = RandomForestClassifier(n_estimators=100, max_depth=7)
+    #
+    # print("Start Training!!!")
+    # rf.fit(training, labels)
 
     testing_sample = []
     for cap in training_captures:
-        path = TEST_PATH  + cap + '.pcap'
+        path = TEST_PATH + cap + '.pcap'
         # print(path)
-        testing_sample.append(packet_extract.extract_pkt(path))
+        testing_sample.append(packet_extract.extract_pkt(path)[0])
 
-    s = [4,1,2,4,5]
-    best_estimators = 100
-    # 2, 3, 2, 3, 3
-
-    rf = RandomForestClassifier(n_estimators=100, max_depth=7, class_weight={0: s[0], 1:s[1],2:s[2], 3: s[3],4: s[4]})
-    rf.fit(training, labels)
     results = []
     for x in range(len(testing_sample)):
         r = rf.predict(testing_sample[x])
         results.append((r == x).sum() / len(r))
-        print(results[-1])
+        #print(results[-1])
 
     print(sum(results) / len(results))
-    print(s)
     # Max
     # accuracy: 0.7302498839618605
     # weights: (4, 1, 2, 4, 5)
--- Packet Analysis/packet_extract.py
+++ Packet Analysis/packet_extract.py
@@ -1,16 +1,12 @@
 import pyshark
 import numpy as np
+import flow as fl
 import os
+
 null_fds = [os.open(os.devnull, os.O_RDWR) for x in range(2)]
 save = os.dup(1), os.dup(2)
-""""
-Explanation of display filter:
-'ip' filters out ipv6 traffic
-'not arp' removes arp requests
-'!(udp.port == 53 or udp.port == 5353 or udp.port == 17)' removes dns, msdns, and dhcp requests
-'!(eth.dst[0] & 1)' removes all broadcast/multicast requests
-"""
-DISPLAY_FILTER = 'ip and not arp and !(udp.port == 53 or udp.port == 5353 or udp.port == 17) and !(eth.dst[0] & 1)'
+
+DISPLAY_FILTER = 'ip and !(arp or icmp) and !(udp.port == 53 or udp.port == 5353 or udp.port == 17) and !(eth.dst[0] & 1) and (tcp or udp)'
 
 
 # Extract the time between packets (in seconds) with pkt_b following after pkt_a
@@ -20,23 +16,55 @@ def pkt_delta(pkt_a, pkt_b):
     return time_b - time_a
 
 
-# Extracts the distance (in seconds) between each packet and their lengths in bytes
-def extract_pkt(path):
+def check_burst(a,b):
+    return pkt_delta(a,b) > 1
+
+
+def extract_pkt(path, src_ip=None):
     os.dup2(null_fds[0], 1)
     os.dup2(null_fds[1], 2)
+    flow_dict = dict()
+    bursts = []
     cap = pyshark.FileCapture(path, display_filter=DISPLAY_FILTER)
     meta = []
+    ips = []
+    burst_index = 0
+    if not src_ip:
+        for pkt in cap:
+            ips.append(str(pkt.ip.src))
+            ips.append(str(pkt.ip.dst))
+
+        src_ip = max([(ips.count(chr), chr) for chr in set(ips)])[1] # get most occuring ip address in capture file
     prev = cap[0]
     for pkt in cap:
-        time_delta = pkt_delta(prev, pkt)
-        length = int(pkt.length)
-        meta.append([time_delta, length])
+        pkt_dict = fl.packet_extract(pkt, src_ip) if src_ip else fl.packet_extract(pkt)
 
+        flow_id = fl.packet_serialize(pkt_dict)
+
+        if flow_id not in flow_dict.keys():
+            flow_dict[flow_id] = fl.Flow()
+
+        flow_dict[flow_id].add_pkt(pkt_dict)
+
+        if not bursts:
+            bursts.append([flow_dict[flow_id].get_analysis(pkt_dict)])
+
+        if check_burst(prev, pkt):
+            burst_index += 1
+            bursts.append([flow_dict[flow_id].get_analysis(pkt_dict)])
+        else:
+            bursts[burst_index].append(flow_dict[flow_id].get_analysis(pkt_dict))
         prev = pkt
 
+        meta.append(flow_dict[flow_id].get_feature_vector())
     os.dup2(save[0], 1)
     os.dup2(save[1], 2)
-    return np.array(meta)
+
+    # for flow in flow_dict.values():
+    #     feature_vector = flow.get_feature_vector()
+    #     meta.append(feature_vector)
+
+    return np.array(meta), bursts
 
 
 # test = extract_pkt('../GatewayVM/recorded_traffic/browser.pcap')
